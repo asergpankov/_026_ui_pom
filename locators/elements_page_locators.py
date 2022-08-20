de7cd122857688bd7449561fd1bacd7f93412a1b
asergpankov <pankovaserg@gmail.com>
2022-08-20 15:09:35 +0300

    added data.py, elements_page.py, elements_page_locators.py, elements_test.py, generator.py

---
+++ locators/elements_page_locators.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+
+class TextBoxPageLocators:
+    FULL_NAME = (By.CSS_SELECTOR, "input#userName")
+    EMAIL = (By.CSS_SELECTOR, "input#userEmail")
+    CURRENT_ADDRESS = (By.CSS_SELECTOR, "textarea#currentAddress")
+    PERMANENT_ADDRESS = (By.CSS_SELECTOR, "textarea#permanentAddress")
+    SUBMIT = (By.CSS_SELECTOR, "button#submit")
+
+    CREATED_FULL_NAME = (By.CSS_SELECTOR, "p#name")
+    CREATED_EMAIL = (By.CSS_SELECTOR, "p#email")
+    CREATED_CURRENT_ADDRESS = (By.CSS_SELECTOR, "p#currentAddress")
+    CREATED_PERMANENT_ADDRESS = (By.CSS_SELECTOR, "p#permanentAddress")
---
+++ tests/elements_test.py
@@ -0,0 +1,15 @@
+from pages.elements_page import TextBoxPage
+
+
+class TestElements:
+    class TestTextBox:
+        def test_text_boxes(self, driver):
+            text_box_page = TextBoxPage(driver, "https://demoqa.com/text-box")
+            text_box_page.open()
+            full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+            output_full_name, output_email, output_current_address, output_permanent_address = text_box_page.check_filled_form()
+            assert full_name == output_full_name
+            assert email == output_email
+            assert current_address == output_current_address
+            assert permanent_address == output_permanent_address
+            # assert input_data == output_data
