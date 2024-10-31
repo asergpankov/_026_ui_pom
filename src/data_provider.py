066f87f60f7bfc58179f15c44b6d356cc7c41651
asergpankov <pankovaserg@gmail.com>
2024-10-31 23:14:05 +0300

    add data_provider time_counter

--- src/data_provider.py
+++ src/data_provider.py
@@ -1,6 +1,9 @@
+import string
+import uuid
 from dataclasses import dataclass, field
+from datetime import datetime, timedelta
 from itertools import product
-from random import choice
+from random import choice, sample
 from typing import Any
 
 from faker import Faker
@@ -10,6 +13,16 @@ fake_eng = Faker("en_US")
 
 
 # Faker.seed(0)
+
+def rand_value(): return str().join(sample(string.ascii_uppercase + string.digits, k=11))
+
+
+def rand_uuid(): return str(uuid.uuid4())
+
+
+def rand_hex(): return str(uuid.uuid4().hex)
+
+
 @dataclass
 class Color:
     colors_list: list
@@ -70,5 +83,30 @@ class Some_Option:
 
 some1 = Some_Option('some1')
 some2 = Some_Option('some2')
-print(some1)
-print(some2)
+
+
+@dataclass
+class DateTime:
+    hour_ago: str
+    day_ago: str
+    week_ago: str
+    now: str
+    hour_ahead: str
+    day_ahead: str
+    week_ahead: str
+
+    @staticmethod
+    def generate_data():
+        date_repr = "%Y-%m-%dT%H:%M:%S"
+        return DateTime(
+            hour_ago=(datetime.now() - timedelta(hours=1)).strftime(date_repr),
+            day_ago=(datetime.now() - timedelta(days=1)).strftime(date_repr),
+            week_ago=(datetime.now() - timedelta(weeks=1)).strftime(date_repr),
+            now=(datetime.now()).strftime(date_repr),
+            hour_ahead=(datetime.now() + timedelta(hours=1)).strftime(date_repr),
+            day_ahead=(datetime.now() + timedelta(days=1)).strftime(date_repr),
+            week_ahead=(datetime.now() + timedelta(weeks=1)).strftime(date_repr)
+        )
+        # cooldown_time = datetime.now() - timedelta(hours=1)
+        # print(cooldown_time.strftime("%Y-%m-%dT%H:%M:%S"))
+        # fake_eng = Faker("ru_RU")
